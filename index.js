164bb2ad84eaf3a2eefcd304bdd13304c8e94332
animesh6219 <62498877+animesh6219@users.noreply.github.com>
2022-10-04 06:46:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ setInterval(() => {
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/pwa-examples/a2hs/sw.js')
+    .register('/school_exam_test/sw.js')
     .then(() => { console.log('Service Worker Registered'); });
 }
 
